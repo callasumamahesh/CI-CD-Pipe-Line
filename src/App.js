088d44c967ad2d6a46ff88921213d1c67df13b08
Umamahesh Banda <bandaumamahesh1919@gmail.com>
2026-01-31 19:14:27 +0530

    removed testing

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div>
       <h1 style={{textAlign:'center',textTransform:'uppercase'}}>CI/Cd Pipe Line</h1> 
-      <p>Develop Branch testing</p>  
+      <p>Develop Branch</p>  
       <Counter />   
     </div>
   )
